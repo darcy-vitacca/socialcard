60238bf3e6db3e4313c95fcf7022af05560b1e4c
Darcy Vitacca <darcy.vitacca@seventhbeam.com>
2022-09-14 21:48:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+[X] - Connect Planetscale and migrate
+[X] - Setup next auth
+[X] - Get linkedin sign in working
+[] - Add sign in and sign out button
+[] - Show auth and unauth and show a profile picture
+[] - Connect to vercel
+[] - Store Social media accounts
+[] - Look into being able to follow programatically from the app without having to visit other social medias
+[] - Show modal to auth into other social medias else hide this message from appearing again
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,34 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import LinkedInProvider from "next-auth/providers/linkedin";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    LinkedInProvider({
+      clientId: env.LINKEDIN_CLIENT_ID ?? "",
+      clientSecret: env.LINKEDIN_CLIENT_SECRET ?? "",
+      client: {
+        token_endpoint_auth_method: "client_secret_post",
+      },
+    })
+  ]
+};
+
+export default NextAuth(authOptions);
